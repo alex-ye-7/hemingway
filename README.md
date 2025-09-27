d966e4dd26b7eee8915d109a9b6eb0b58e83a4c9
Alex Ye <alexye@Alexs-MacBook-Air-3.local>
2025-09-27 15:00:01 +0800

    bigram language

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # HemingwayBot
-intro to neural networks
+
+## Alexander Ye
+
+Code based on Andrej Karpathy Neural Networks Zero to Hero Playlist on Youtube
+
+Interested in how large language models work under the hood
+
+Created a language model that models Ernest Hemingway's writing
+
+Without a GPU, how close can I get?
+
+## bigramLM.py
+
+A bigram language model assumes that the probability of a word (in this case, one character) depends only on the word immediately preceding it.
+
+For example, "I love cats" has bigrams (I, love) and (love, cats).
+
+### Implementation Details
+**get_batch(split):** Generates a small batch of inputs and outputs for our model based on block_size, or how much context we want to give for next character
+
+So for example, if we have a block of tokens encoded as tensor([67, 64, 28, 16, 13,  1, 21,  9, 26]):
+- When context is tensor([67]) target is tensor(64)
+- When context is tensor([67, 64]) target is tensor(28)
+- When context is tensor([67, 64, 28]) target is tensor(16)
+- When context is tensor([67, 64, 28, 16]) target is tensor(13)
+- When context is tensor([67, 64, 28, 16, 13]) target is tensor(1)
+- When context is tensor([67, 64, 28, 16, 13,  1]) target is tensor(21)
+- When context is tensor([67, 64, 28, 16, 13,  1, 21]) target is tensor(9)
+- When context is tensor([67, 64, 28, 16, 13,  1, 21,  9]) target is tensor(26)
---
+++ bigramLM.py
@@ -0,0 +1,93 @@
+# Alexanader Ye 2025 
+
+# A bigram language model looks at one token (in this case, one character)
+# and simply tries to predict the next
+
+import torch
+import torch.nn as nn
+from torch import functional as F
+
+# Hyperparameters
+batch_size = 4 # how many indepedent sequences can you process in parallel?
+block_size = 8 # how big of a context do you want to train on?
+max_iters = 3000 # for training
+learning_rate = 1e-2
+
+torch.manual_seed(1337)
+
+with open("./data/snows.txt", "r", encoding='utf-8') as f:
+    content = f.read()
+
+# Create encoding functions for our model to understand
+chars = sorted(list(set(content)))
+vocab_size = len(chars)
+char_to_i = {char : i for i, char in enumerate(chars)} # mapping from string to int
+i_to_char = {i : char for i, char in enumerate(chars)} # reverse map
+encode = lambda s: [char_to_i[char] for char in s]  # take a string, output int array
+decode = lambda a: ''.join([i_to_char[i] for i in a]) # take an int array, output a string
+
+# Train and test split
+data = torch.tensor(encode(content), dtype=torch.long)
+n = int(len(data) * 0.9) # first 90% will be training
+train_data = data[:n]
+val_data = data[n:]
+
+# For training, setting up input x and target y's
+def get_batch(split):
+    """
+    Output shapes will be (batch_size, block_size) or (B, T) 
+    Think of x as 4 randomly sampled blocks in the text
+    Think of y as the +1 offset of each sampled block
+    So that target y at position (i,j) has its context in row/batch i in the first j columns/blocks
+    """
+    data = train_data if split == "train" else val_data
+    ix = torch.randint(len(data)-block_size, (block_size,)) # sampling 4 blocks within text
+    x = torch.stack([data[i:i+block_size] for i in ix])
+    y = torch.stack([data[i+1:i+block_size+1] for i in ix])
+    return x, y
+
+class BigramLanguageModel(nn.Module):
+    def __init__(self, vocab_size):
+        super().__init__()
+        self.token_embedding_table() = nn.Embedding(vocab_size, vocab_size)
+
+    def forward(self, idx, targets=None):
+        # idx and targets are both (B, T) tensor of integers that get_batch creates
+        logits = self.token_embedding_table(idx) # logits is therefore (B,T,vocab_size) or (B,T,C)
+
+        if targets is None:
+            loss = None
+        else:
+            # Cross entropy loss takes channel as second argument in shape -> reshape
+            # Torch view can make it desired shape
+            B, T, C = logits.shape
+            logits = logits.view(B*T, C) 
+            targets = targets.view(B*T) 
+            loss = F.cross_entropy_loss(logits, targets)
+
+        return logits, loss
+    
+    def generate(self, idx, max_new_tokens=1000):
+        # Take a (B,T) and make it (B,T+1), (B,T+2), ..., (B, T+max)
+        for _ in range(max_new_tokens):
+            logits, loss = self(idx) # Pytorch trick equivalent to self.forward(idx)
+            logits = logits[:, -1, :] # Focus on last token for each batch item, shape (B,C)
+            probs = F.softmax(logits, dim=-1) # dim -1 means along the last dimension (vocab_size), so each batch is indepdent
+            idx_next = torch.multinomial(probs, num_samples=1) # sample from distribution
+            idx = torch.cat((idx, idx_next), dim=1) # Along time dimension
+        return idx
+    
+model = BigramLanguageModel(vocab_size)
+optimizer = torch.optim.AdamW(model.parameters(), lr=learning_rate)
+ 
+# Training loop
+for iter in range(max_iters):
+    xb, yb = get_batch('train')
+    logits, loss = model(xb, yb)
+    optimizer.zero_grad(set_to_none=True)
+    loss.backward()
+    optimizer.step()
+
+# Generate from model
+idx = torch.zeros((1,1), dtype=torch.long) # This starting point limits our batch to one sequence
+print(decode(model.generate(idx, 400)[0].tolist())) # tolist to convert from tensor to list
